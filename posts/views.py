d8b313ba86c9eb7b70417085a38c391679a46fc2
Nathan Ortiz <nathanlucianortiz@gmail.com>
2021-01-28 20:17:57 -0600

    added css and templates

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, TemplateView
 from .models import Post
 
 
@@ -7,3 +9,5 @@ class HomePageView(ListView):
     template_name = 'home.html'
     context_object_name = 'all_posts_list'
 
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
